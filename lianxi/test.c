d105560d4cc0688c6385463f3ecdaff9d4a04108
xiaoan001 <1270949404@qq.com>
2022-04-23 18:05:38 +0800

    习题完

--- lianxi/test.c
+++ lianxi/test.c
@@ -1,120 +1,95 @@
+#define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-//int main()
-//{
-//	int a[5] = { 5,4,3,2,1 };
-//	int* ptr = (int*)(&a + 1);
-//	printf("%d ,%d", *(a + 1), *(ptr - 1));
-//	return 0;
-//}
-//int main()
-//{
-//	int aa[2][5] = { 10,9,8,7,6,5,4,3,2,1 };
-//	int* ptr1 = (int*)(&aa + 1);
-//	int* ptr2 = (int*)(*(aa + 1));
-//	//aa是个数组，aa+1 是第二行首元素地址，*（aa+1）==aa[1]表示5这个元素地址，再强制类型转换为int*
-//	printf("%d,%d", *(ptr1 - 1), *(ptr2 - 1));
-//	return 0;
-//
-//}
-
-
-//题目内容：
-//实现一个函数，可以左旋转字符串中的k个字符。
-//例如：
-//ABCD左旋转一个字符得到BCDA
-//ABCD钻旋转两个字符得到CDAB
-
-//旋转字符串
-// 
-//
-//暴力求解法
 #include<string.h>
-#include<assert.h>
-//void left_move(char* arr[],int k)
+//int is_left_move(char* str1, char* str2)
 //{
-//	assert(arr != NULL);
-//	int i = 0;
-//	int len = strlen(arr);
-//	for (i = 0; i < k; i++)
+//	//1.在str1后边追加一个str1字符串
+//	//strcat   自己给自己追加时候不能用strcat函数
+//	// strncat
+//	int len1 = strlen(str1);
+//	int len2 = strlen(str2);
+//	strncat(str1,str1,6);//abcdefabcdef
+//	if (len1 != len2)
+//		return 0;
+//	//strstr函数时一个找子串
+//	//2.判断str2指向的字符串是否时str1指向的字符串的子串
+//	char*ret=strstr(str1, str2);//在str1中找str2子串
+//	if (ret == NULL)
 //	{
-//		//左旋转一个字符
-//		// 1
-//		char tmp = *arr;
-//		//2
-//
-//		int j = 0;
-//		for (j = 0; j < len-1; j++)
-//		{
-//			*(arr + j) = *(arr + j + 1);
-//		}
-//		//3
-//		*(arr + len - 1) = tmp;
+//		return 0;
 //	}
-//}
-//三步翻转法
-// abcdef
-//分开 ab cdef
-//再各自翻转 ba fedc
-//合起来 bafedc
-//再翻转 cdefab
-//void reverse(char* left, char* right)
-//{
-//	assert(left != NULL);
-//	assert(right != NULL);
-//	while (left<right)
+//	else
 //	{
-//		char tmp = *left;
-//		*left = *right;
-//		*right = tmp;
-//		left++;
-//		right--;
+//		return 1;
 //	}
-//}
-//void left_move(char*arr,int k)
-//{  
-//	//k表示传过来逆序几位
-//	int len = strlen;
-//	assert(arr);
-//	assert(k <= len);
-//	reverse(arr,arr+k-1);//逆序左边   reverse是一个逆序函数
-//	reverse(arr+k,arr+len-1);//逆序右边
-//	reverse(arr,arr+len-1);//逆序整体
 //}
 //int main()
-//{ 
-//	char arr[] = "abcdef";
-//	left_move(arr, 2);
-//	printf("%s\n", arr);
+//{
+//	char arr1[30] = "abcdef";
+//	char arr2[] = "cdefab";
+//    int ret=is_left_move(arr1, arr2);
+//	if (ret == 1)
+//	{
+//		printf("yes\n");
+//	}
+//	else
+//	{
+//		printf("No\n");
+//	}
 //	return 0;
 //}
 //
-int left_move(char* s1, char* s2)
+///杨氏矩阵
+//题目内容
+// 有一个数字矩阵，矩阵的每一行从左到右是递增的，矩阵从上到下是递增的
+// 要求复杂度小于O（n）
+//1 2 3
+//2 3 4
+//3 4 5 
+//
+int FindNum(int arr[3][3], int k, int*px, int*py)
 {
-	int len = strlen(s1);
-	int i = 0;
-	for (i = 0; i < len; i++)
+	int x = 0;
+	int y = *py - 1;
+	while (x <=*px - 1 && y >= 0)
 	{
-		left_move(s1, 1);
-		int ret=strcmp(s1, s2);
-		if (ret == 0)
-			return;
+		if (arr[x][y] > k)//右上角元素比较
+		{
+			y--;
+		}
+		else if (arr[x][y] < k)
+		{
+			x++;
+		}
+		else
+		{
+			*px = x;
+			*py = y;
+			return 1;
+		}
+
 	}
+	//找不到
 	return 0;
 }
 int main()
 {
-	char* arr1 = "abcdef";
-	char* arr2 = "cdefad";
-	int ret = left_move(arr1, arr2);
+	int arr[3][3] = { {1,2,3},{4,5,6},{7,8,9} };
+	int k = 7;
+	int x = 3;
+	int y = 3;
+	//返回型参数
+	int ret = FindNum(arr, k, &x, &y);
 	if (ret == 1)
-		printf("yes\n");
+	{
+		printf("找到了\n");
+	}
 	else
-		printf("NO\n");
+	{
+		printf("找不到\n");
+	}
 	return 0;
 }
-
-
-
 
 
 
