fe37972b048ac94c6b43547fdc04e3ddd880ca86
xiaoan001 <1270949404@qq.com>
2022-04-28 17:01:08 +0800

    字符函数50

--- lianxi/test.c
+++ lianxi/test.c
@@ -1,94 +1,64 @@
-#define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+//int strncmp(const char* string1, const char* string2, size_t count);
+//count  字节个数
 #include<string.h>
 #include<assert.h>
-//求字符串长度
-//strlen
-//计数器方法
-//递归
-//指针减指针
-//size_t==unsigned int 
-// size_t不会返回负数  int会返回负数
-//strlen 返回类型是size_t
-//
-//int my_strlen(const char* str)
-//{
-//	int count = 0;
-//	assert(str != NULL);
-//	while (*str != '\0')
-//	{
-//		count++;
-//		str++;
-//	}
-//	return count;
-//}
 //int main()
 //{
-//	int len = strlen("ancdef");
-//	////错误示范
-//	//char arr[] = { 'a','b','c','d','e','f' };
-// 	//int len = strlen(arr);
-//	printf("%d\n", len);
-//}
-//strcpy  字符串拷贝
-//char* my_strcpy(char* dest, char* src)
-//{
-//	assert(dest != NULL && src != NULL);
-//	char* ret = dest;
-//	//拷贝src指向字符串到dest指向的空间，包含'\0'
-//	while (*dest++ = *src++)
-//	{
-//		;
-//		//*dest++= *src++;
-//		//拷贝bit
-//	}
-//	//返回目的空间的起始地址
-//	return ret;
-//	//*dest = *src;//拷贝'\0'
-//
-//}
-//int main()
-//{
-//	char arr1[] = "abcdef";
-//	char arr2[] = "bit";
-//	//错误示范
-//	//char arr[] = { 'b','i','t' };没有\0位置
-//	my_strcpy(arr1, arr2);
-//	printf("%s\n", arr1);
+//	 //strncap  字符串比较
+//	const char* p1 = "abzerf";//加const  是考虑常量字符串，本身是不能被修改的
+//	const char* p2 = "abcqwer";
+//	//int ret=strcmp(p1, p2);
+//	int ret = strncmp(p1, p2, 4);//3表示p1和p2中只比较前三个字符串
+//	printf("%d\n", ret);
 //	return 0;
-//
 //}
-
-
-
-//strcat字符串追加
-char* my_strcat(char*dest,const char*src)
-{              //目的地     源头
-	
-	char* ret = dest;
-	assert(dest != NULL);
-	assert(src);
-	//找到目的字符串'\0'
-	while (*dest != '\0')
+///strstr  查找子字符串
+//NULL-空指针
+//   NUL/Null--  \0
+char*my_strstr(const char* p1, const char* p2)
+{
+	assert(p1 != NULL);
+	assert(p2 != NULL);
+    const char* s1 = p1;
+	const char* s2 = p2;
+	char* cur =(char*) p1;
+	if (*p2 == '\0')//如果p2为空字符串的话，则返回\0
 	{
-		dest++;
+		return (char*)p1;
 	}
-	//追加
-	while (*dest++ = *src++)
+	while (*cur)//这里*cur表示*cur解引用后是否是\0
 	{
-		;
+		s1 = cur;
+		s2 = (char*)p2;
+		while( (*s1!='\0')&&(*s2 != '\0') && (*s1 == *s2))
+		{
+			s1++;
+			s2++;
+		}
+		if (*s2 == '\0')
+		{
+			return cur;//找到子串
+		}
+		cur++;
 	}
-	return ret;
+	return NULL;//找不到子串
 }
 int main()
 {
-	char arr1[30] = "hello";
-	char arr2[] = "world";
-	my_strcat(arr1, arr2);//源头 arr2拷贝到目的地arr1
-	printf("%s\n", arr1);
+	char* p1 = "abcdefghi";
+	char* p2 = "def";
+	char*ret=my_strstr(p1, p2);//在p1中找p2的字符串，如果存在，则会打印p2中寻找元素及后面字符
+	if (ret == NULL)
+	{
+		printf("子串不存在");
+	}
+	else
+	{
+		printf("%s\n", ret);
+	}
+	return 0;
 }
-
-
 
 
 
