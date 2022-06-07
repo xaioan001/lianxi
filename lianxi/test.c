be976bd39be2a46f3380b9adeba6b58e7f0357d4
xiaoan001 <1270949404@qq.com>
2022-06-07 17:33:48 +0800

    动态内存存放

--- lianxi/test.c
+++ lianxi/test.c
@@ -2,160 +2,166 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<windows.h>
-//错误案例
+//char* GetMemory(void)
+//{
+//	char p[] = "hello world";  //p数组 hello world\0
+//	return p;//出函数p这块空间还给操作系统，下面调用就非法访问
 // 
-// 
-// 
-//int main()
-//{
-//	/*int* p = (int*)malloc(40);
-//	if (p = NULL)
-//	{
-//		return 0;
-//
-//	}
-//	int* p2 = realloc(p, 80);
-//	if (p2 != NULL)
-//	{
-//		p = p2;
-//	}*/
-//	int* p = realloc(NULL, 40);//malloc
-//	
-//	
-//	
-//	return 0;
 //}
-//int main()
-//{
-	////1.对NULL指针解引用
-	//int* p = malloc(40);
-	////p进行相关的判断
-	//*p = 10;//malloc开辟空间失败有可能对NULL指针解引用
-	////对动态开辟内存的越界访问
-	//int* p = (int*)malloc(40);
-	//if (p == NULL)
-	//{
-	//	return 0;
-	//}
-	//int i = 0;
-	////越界
-	//for (i = 0; i <= 10; i++)
-	//{
-	//	*(p + i) = i;//越界访问
-	//}
-	//free(p);
-	//p = NULL;
-	//4. 使用free释放动态开辟内存的一部分
-	//int* p = (int*)malloc;
-	//if(p == NULL)
-	//{
-	//	return 0;
-	//}
-	//int i = 0;
-	//for (i = 0; i < 10; i++)
-	//{
-	//	*p++ = i;
-	//}
-	////回收空间
-	////4. 使用free释放动态开辟内存的一部分 
-	//free(p);//p如果变化释放的只是当前位置空间，p不变释放的是刚开始开辟的空间
-	//p = NULL;
-	//return 0;
-	
-	
-	//5.对同一块块动态内存的多次释放
-
-	//int* p = (int*)malloc(40);
-	//if (p == NULL)
-	//{
-	//	return 0;
-	//}
-	////使用
-	////释放
-	//free(p);
-	//p = NULL;//p置为空指针对下面再次释放，下面无效
-	//free(p);
-	////谁申请谁释放
-
-
-
-	//6.对动态开辟内存忘记释放（内存泄露）
-	/*while (1)
-	{
-		malloc(1);
-	}
-
-	return 0;*/
-    
-
-//}
-//void GetMemory(char* p)
-//{
-//	p = (char*)malloc(100);
-//}
-//void Test(void)
-//{
-//	char* str = NULL;//str中放了一个空指针
-//	GetMemory(str);//调用完之后GetMemory中依旧方的是空指针
-//	strcpy(str, "hello world");//程序奔溃
-//	printf(str);
-//	//运行代码程序会出现奔溃的现象
-//	//程序存在内存泄露问题
-//	//str以值传递的形式给p
-//	//p是GetMemory函数返回之后，动态开辟内存尚未释放
-//	//并且无法找到，所以会造成内存泄露
-//}
-//int main()
-//{
-//	Test();
-//	/*char* str = "abcdef";
-//	printf("%s\n", str); */
-//	return 0;
-//}
-
-//改正一
-//void GetMemory(char* *p)
-//{
-//	*p = (char*)malloc(100);//把开辟好的地址放在*p中
-// }
 //void Test(void)
 //{
 //	char* str = NULL;
-//	GetMemory(&str);
-//	strcpy(str, "hello world");
+//	str = GetMemory();
 //	printf(str);
-//	free(str);
-//	str = NULL;
 //}
 //int main()
 //{
 //	Test();
+//	return 0;  
+//}
+//int* test()
+//{
+//	int a = 10;//栈区
+//	return &a;//出函数空间还给操作系统
+//}
+//int main()
+//{
+//	int* p = test();
+//	*p = 20;//非法访问
+//}
+//int* test()
+//{
+//	static int a = 10;//静态区
+//	return &a;//static修饰生命周期变长
+//}
+//int main()
+//{
+//	int* p = test();
+//	*p = 20;
 //	return 0;
 //}
-char* GetMemory(char* p)
+//int* test()
+//{
+//	int* ptr = malloc(100);//堆区
+//	return ptr;
+//}
+//int main()
+//{
+//	int* p = test();
+//	return 0;
+//}
+//void Test(void)
+//{
+//	char* str = (char*)malloc(100);
+//	strcpy(str, "hello");//str中放hello\0的地址
+//	free(str);//开辟的空间还给操作系统 ，虽然str中存储hello\0地址，但不能访问
+//	if (str != NULL)//free不会把str置为空指针
+//	{
+//		strcpy(str, "world");
+//		printf(str);
+//	}
+//}
+//int main()
+//{
+//	Test();
+//	return 0; 
+//}
+
+
+
+
+//柔性数组
+//c99中，结果中的最后一个元素允许是未知大小的数组，这就叫做【柔性数组】成员。
+//struct S
+//{
+//	int n;
+//	int arr[];//未知大小的--柔性数组成员--数组大小是可以调整的
+//};
+//int main()
+//{
+//	//struct S s;
+//	//printf("%d\n", sizeof(s));//在计算结构体大小时，不包含柔性数组大小
+//	struct S* ps =(struct S*) malloc(sizeof(struct S)+5*sizeof(int));
+//	ps->n = 100;
+//	int i = 0;
+//	for (i = 0; i < 5; i++)
+//	{
+//		ps->arr[i] = i;//0,1,2,3,4
+//	}
+//	struct S* ptr=realloc(ps, 44);
+//	if (ptr != NULL)
+//	{
+//		ps = ptr;
+//	}
+//	for (i = 5; i < 10; i++)
+//	{
+//		ps -> arr[i] = i;
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d", ps->arr[i]);
+//	}
+//	free(ps);
+//	ps = NULL;
+//	return 0;
+//}
+//struct S
+//{
+//	int n; 
+//	int* arr;
+//};
+//int main()
+//{
+//	struct S* ps = (struct S*)malloc(sizeof(struct S));
+//	ps->arr = malloc(5 * sizeof(int));
+//	int i = 0;
+//	for (i = 0; i < 5; i++)//先存放
+//	{
+//		ps->arr[i] = i;
+//	}
+//	for (i = 0; i < 5; i++)
+//	{
+//	  printf("%d",ps->arr[i] = i);//再打印
+//	}
+//	//调整大小
+//	int*ptr= realloc(ps->arr, 10 * sizeof(int));
+//	if (ptr != NULL)
+//	{
+//		ps->arr = ptr;
+//	}
+//	for (i = 5; i < 10; i++)
+//	{
+//		ps->arr[i] = i;
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d", ps->arr[i]);
+//	}
+//	//释放内存
+//	free(ps->arr);
+//	ps->arr = NULL;
+//	free(ps);
+//	ps = NULL;
+//	return 0;
+//}
+
+
+
+
+//结构中的柔性数组成员前面必须至少一个其他成员
+//sizeof返回的这种结构体大小不包括柔性数组的内存
+//包含柔性数组成员的结构体malloc()函数进行内存动态分配，并且分配的内存应该大于结构体的大小，以适应柔性数组的预期大小
+struct S
 {
-	p = (char*)malloc(100);
-	return p;
-}
-void Test(void)
-{
-	char* str = NULL;
-	str=GetMemory(str);
-	strcpy(str, "hello world");
-	printf(str);
-	free(str);
-	str = NULL;
-}
+	int n;
+	char c;
+	int arr[0];//柔性数组成员
+};
 int main()
 {
-	Test();
+	printf("%d", sizeof(struct S));
 	return 0;
 }
-
-
-
-
-
 
 
 
